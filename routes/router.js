e78063ee57b2573ba7cd4cbe35dc7cef6244e669
Michael McFerran <67926227+MichaelMcFerran@users.noreply.github.com>
2021-11-24 15:23:05 +0000

    routing changes

--- routes/router.js
+++ routes/router.js
@@ -14,5 +14,52 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Express' });
 });
 
+// Login Authentification
+// login script will check the MySQL dB if details match
+// login form action is to 'auth', creates user and pass consts with entered values
+// checks the details match by querying the accounts table in the dB accounts table
+router.post('/auth', (request, response) => {
+  // Get values submitted in POST form
+  const { username, password } = request.body;
+  if (username && password) {
+   const userDetailsSQL =
+    "SELECT * FROM user WHERE username = '" +
+    username +
+     "' AND password = '" +
+     password +
+    "'";
+   dbConnection.query(userDetailsSQL, (error, results) => {
+    console.log(results); // used to debug
+    // change below to a conditional for username
+    if (results.length > 0) {
+     request.session.loggedin = true; // checks if result is returned, changed from sess =
+     request.session.username = username; // passed session var between page routing when user is logged on
+     request.session.password = password;
+     response.redirect('/index'); // redirect to correct index homepage, URL/index is displayed on browser. app.get is needed to find + the page
+    } else {
+     response.send('Incorrect Username and/or Password!');
+    }
+    response.end();
+   });
+  } else {
+   response.send('Please enter Username and Password!');
+   response.end();
+  }
+ });
+ 
+ // index router to homepage if login authentification is achieved, this is intermediate page between login and index/dashboard
+ router.get('/index', (request, response) => {
+  if (request.session.loggedin) {
+   console.log(
+    request.session.username // grabs username so user can be addressed in the subsequent web pages
+   );
+ 
+   // put session grabbing vars here specific to user?
+   response.render('index', { title: "" + request.session.username + "'s Hub" }); // render page only on successful login
+  } else {
+   response.send('Please login to view this page!', request.session.username);
+  }
+  response.end();
+ });
 
 module.exports = router;
