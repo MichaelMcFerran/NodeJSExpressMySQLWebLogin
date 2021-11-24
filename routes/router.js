c2dbe029dc8e439727d36a18f410a8bce0da0762
Michael McFerran <67926227+MichaelMcFerran@users.noreply.github.com>
2021-11-24 15:17:56 +0000

    fixed page routings

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,18 @@
+/*
+This file handles all page routings and HTTP requests
+*/
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page as home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
 
 module.exports = router;
